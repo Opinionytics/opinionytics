d353dae5260ff4e44a22535d93ee8fc2de6ccd01
Amine M. Boulouma <amine@boulouma.com>
2018-05-17 21:15:33 +0200

    Added contact template with ajax handler

--- opinionytics/static/js/ajax.js
+++ opinionytics/static/js/ajax.js
@@ -53,3 +53,22 @@ $("#signUpForm").submit(function(event) {
         alert("Erreur réseau. Veuillez vérifier votre connexion.");
     });
 });
+
+
+$("#contactForm").submit(function(event) {
+    event.preventDefault();
+    var form = $(event.target);
+    var formData = form.serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: '/contact/',
+        data: formData,
+        timeout: 100000
+    }).done(function(response) {
+        console.log(response);
+    }).fail(function(response) {
+        console.log(response);
+        alert("Erreur réseau. Veuillez vérifier votre connexion.");
+    });
+});
