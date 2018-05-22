6f5823797702e6bdccd61da718c6bb7ddc800520
Amine M. Boulouma <amine@boulouma.com>
2018-05-22 17:43:42 +0200

    Update models.py

--- opinionytics/models.py
+++ opinionytics/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 
-class MonthlyWeatherByCity(models.Model):
-    month = models.IntegerField()
-    boston_temp = models.DecimalField(max_digits=5, decimal_places=1)
-    houston_temp = models.DecimalField(max_digits=5, decimal_places=1)
-
-
-class DailyWeather(models.Model):
-    month = models.IntegerField()
-    day = models.IntegerField()
-    temperature = models.DecimalField(max_digits=5, decimal_places=1)
-    rainfall = models.DecimalField(max_digits=5, decimal_places=1)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2)
-
-
 class Popularity(models.Model):
     concept = models.CharField(max_length=50)
     date = models.CharField(max_length=50)
