71ea7838ef1c181275622f4f29ef7dcbc66b3c15
Amine M. Boulouma <amine@boulouma.com>
2018-05-22 05:34:46 +0200

    Updated get results from text

--- opinionytics/views.py
+++ opinionytics/views.py
@@ -121,7 +121,44 @@ def get_result_text(request):
         text = request.POST.get('textfield', None)
         if text != None:
             if len(text.split(" ")) > 50:
-                return render(request, 'result.html', all_features_view.execute_text(text))
+                analyze = all_features_view.execute_text(text)
+                summary = analyze['summary']
+                Popularity.objects.all().delete()
+                Polarity.objects.all().delete()
+                Topics.objects.all().delete()
+                Subjectivity.objects.all().delete()
+                polarity = analyze['positivity']
+                topics = analyze['topics']
+                subjectivity = analyze['subjectivity']
+
+                for concepts in analyze['popularity']:
+                    concept = concepts["concept"]
+                    _concept = concept
+                    for date, score in concepts["popularity"].items():
+                        Popularity.objects.create(concept=concept, date=str(date).split()[0], score=score)
+
+                Polarity.objects.create(polarity=polarity['polarity'], confidence=polarity['polarity_confidence'])
+                _polarity = polarity=polarity['polarity']
+
+                _topics = []
+                for concept in topics:            
+                    Topics.objects.create(topics=concept['label'], confidence=concept['confidence'])
+                    _topics.append(concept['label'])
+
+                Subjectivity.objects.create(subjectivity=subjectivity['subjectivity'], confidence=subjectivity['subjectivity_confidence'])
+                _subjectivity = subjectivity['subjectivity']
+
+                chart_list = all_charts(request)
+
+                return render(request, 'result.html', {  
+                    'concept_' : _concept,
+                    'topics_': [t for t in _topics],
+                    'subjectivity_': _subjectivity,
+                    'polarity_': _polarity,
+                    'summary' : summary,
+                    'chart_list' : chart_list,
+                    }
+                )
             else:
                 return HttpResponseRedirect("../analyze/")
         
