a8c81ebfac88a0569fa45f81ab54e4b759379301
Amine M. Boulouma <amine@boulouma.com>
2018-10-18 00:20:13 +0200

    Cleaned project with PEP8

--- opinionytics/urls.py
+++ opinionytics/urls.py
@@ -1,27 +1,9 @@
-"""Opinionitycs URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.urls import include, path
+from django.urls import path
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
 
 from . import views
 
 urlpatterns = [
-
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
     path('services', views.services, name='services'),
@@ -39,5 +21,4 @@ urlpatterns = [
     path('get_result_url', views.get_result_url, name='result-url'),
     path('contact', views.contact, name='contact'),
     path('admin', admin.site.urls)
-
 ]
